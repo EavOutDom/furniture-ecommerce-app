56dc55f2bc5c37b2ca9f4598f24388a9650d5014
EavOutDom <eavoutdom400@gmail.com>
2023-07-28 13:20:03 +0700

    styles outsite component

---
+++ screens/home.style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "../constants/theme";
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontFamily: 'bold',
+    fontSize: 20
+  }
+});
+
+export default styles;
