29b7fb2ce7d4e25ddafce8fbab2cc0e624c71f14
EavOutDom <eavoutdom400@gmail.com>
2023-07-31 09:35:29 +0700

    home screen search bar

--- components/home/Welcome.jsx
+++ components/home/Welcome.jsx
@@ -1,17 +1,42 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import styles from "./welcome.style";
 import { COLORS, SIZES } from "../../constants/theme";
+import { Feather, Ionicons } from "@expo/vector-icons";
+import { TextInput } from "react-native";
 
 const Welcome = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcomeTxt(COLORS.black, SIZES.xSmall)}>
-        Find The Most
-      </Text>
-      <Text style={styles.welcomeTxt(COLORS.primary, 0)}>
-        Luxurious Furniture
-      </Text>
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.welcomeTxt(COLORS.black, SIZES.xSmall)}>
+          Find The Most
+        </Text>
+        <Text style={styles.welcomeTxt(COLORS.primary, 0)}>
+          Luxurious Furniture
+        </Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <TouchableOpacity>
+          <Feather name="search" size={24} style={styles.searchIcon} />
+        </TouchableOpacity>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            value=""
+            onPressIn={() => {}}
+            placeholder="what are you looking for"
+            style={styles.searchInput}
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn}>
+          <Ionicons
+            name="camera-outline"
+            color={COLORS.offwhite}
+            size={SIZES.xLarge}
+          />
+        </TouchableOpacity>
+      </View>
+      <View></View>
     </View>
   );
 };
--- components/home/welcome.style.js
+++ components/home/welcome.style.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import { SIZES } from "../../constants/theme";
+import { COLORS, SIZES } from "../../constants/theme";
 
 const styles = StyleSheet.create({
   container: {
@@ -7,11 +7,45 @@ const styles = StyleSheet.create({
   },
   welcomeTxt: (color, top) => ({
     fontFamily: 'bold',
-    fontSize: SIZES.xxLarge - 5,
+    fontSize: SIZES.xxLarge - 6,
     marginTop: top,
-    marginHorizontal: 12,
+    marginHorizontal: SIZES.small,
     color: color
-  })
+  }),
+  searchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.secondary,
+    height: 50,
+    borderRadius: SIZES.medium,
+    marginVertical: SIZES.medium,
+    marginHorizontal: SIZES.small
+  },
+  searchIcon: {
+    marginHorizontal: 10,
+    color: COLORS.gray
+  },
+  searchWrapper: {
+    flex: 1,
+    marginRight: SIZES.small,
+    borderRadius: SIZES.small,
+    backgroundColor: COLORS.secondary
+  },
+  searchInput: {
+    fontFamily: 'regular',
+    height: '100%',
+    width: '100%',
+    paddingHorizontal: SIZES.small
+  },
+  searchBtn: {
+    width: 50,
+    height: '100%',
+    backgroundColor: COLORS.primary,
+    borderRadius: SIZES.medium,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 export default styles;
